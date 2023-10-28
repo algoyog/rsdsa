6041110fc47650d9e7bab55263f649a20152dbad
Aravind B <aravindb@Aravinds-MacBook-Pro.local>
2023-10-28 11:40:49 +0530

    Cleanup

--- dsa/src/main.rs
+++
@@ -1,9 +0,0 @@
-mod ds;
-use ds::linkedlist;
-
-fn main() {
-    println!("Hello, world!");
-    linkedlist::print();
-    let n = linkedlist::init_linkedlist("in");
-    println!("The val returned: {:?}",n.val);
-}
